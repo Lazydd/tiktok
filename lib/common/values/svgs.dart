5893a47925a00b0c72ad32f7c9b6c3cc0a441f8e
lazy <ddlazy@qq.com>
2024-10-12 16:30:20 +0800

    增加svg图片

--- lib/common/values/svgs.dart
+++ lib/common/values/svgs.dart
@@ -2,4 +2,8 @@ class AssetsSvgs {
   static const pauseSvg = 'assets/svg/pause.svg';
   static const wechat = 'assets/svg/wechat.svg';
   static const qq = 'assets/svg/qq.svg';
+  static const commonExpandBackSvg = 'assets/svg/common_expand_back.svg';
+  static const myCloseSvg = 'assets/svg/my_close.svg';
+  static const refreshSvg = 'assets/svg/refresh.svg';
+  static const mySettingSvg = 'assets/svg/my_setting.svg';
 }
