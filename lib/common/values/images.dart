59e0922153d20712b3ea2aed780243246089b0a6
Lazydd <71319962+Lazydd@users.noreply.github.com>
2025-07-10 16:48:55 +0800

    Update images.dart

--- lib/common/values/images.dart
+++ lib/common/values/images.dart
@@ -1,8 +1,8 @@
 class AssetsImages {
   static const avatarPng = 'assets/images/guide.jpg';
-  static const msgIcon6 = 'tiktok/assets/images/msg-icon6.webp';
-  static const msgIcon7 = 'tiktok/assets/images/msg-icon7.webp';
-  static const msgIcon8 = 'tiktok/assets/images/msg-icon8.webp';
-  static const msgIcon9 = 'tiktok/assets/images/msg-icon9.webp';
-  static const redHeart = 'tiktok/assets/images/red_heart.webp';
+  static const msgIcon6 = 'assets/images/msg-icon6.webp';
+  static const msgIcon7 = 'assets/images/msg-icon7.webp';
+  static const msgIcon8 = 'assets/images/msg-icon8.webp';
+  static const msgIcon9 = 'assets/images/msg-icon9.webp';
+  static const redHeart = 'assets/images/red_heart.webp';
 }
