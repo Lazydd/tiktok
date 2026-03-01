8ee3de62c0bac29c2b2ccdd222bd467b6b05c467
lazy <ddlazy@qq.com>
2026-03-01 19:19:10 +0800

    修正图片地址

--- lib/pages/home/share/controller.dart
+++ lib/pages/home/share/controller.dart
@@ -4,7 +4,7 @@ class ShareController extends GetxController {
   ShareController();
 
   static const List<Map<String, dynamic>> topList = [
-    {"name": "aaaa", "icon": 'https://dy.ttentau.top/assets/0-DZZhXKhG.png'},
+    {"name": "aaaa", "icon": '${Constants.imagesUrl}/assets/0-DZZhXKhG.png'},
     {"name": "加微信朋友", "icon": IconFont.wechat, "type": "wechat"},
     {"name": "更多", "icon": Icons.chevron_right, "type": "more"},
   ];
